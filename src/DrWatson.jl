5d0447414e4356ae292d5f79f9bfd40306a498f3
George Datseris <datseris.george@gmail.com>
2019-02-04 15:20:36 +0100

    revert previous commit, it was a bad idea

--- src/DrWatson.jl
+++ src/DrWatson.jl
@@ -7,10 +7,10 @@ using Pkg
 export projectdir, datadir, srcdir, plotsdir
 export projectname
 
-projectdir() = dirname(Base.active_project())
-datadir() = projectdir()*"/data"
-srcdir() = projectdir()*"/src"
-plotsdir() = projectdir()*"/plots"
+projectdir() = dirname(Base.active_project())*"/"
+datadir() = projectdir()*"data/"
+srcdir() = projectdir()*"src/"
+plotsdir() = projectdir()*"plots/"
 
 projectname() = Pkg.REPLMode.promptf()[2:end-7]
 
