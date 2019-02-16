bd6563972e8059846f6c6bd7d43ed9d889dcf1b6
George Datseris <datseris.george@gmail.com>
2019-02-16 17:04:26 +0100

    draft of produce_or_load (#8)
    * named tuple macro
    
    * documentation for @ntuple
    
    * add tests for @ntuple
    
    * draft of produce_load
    
    * put FileIO functionality under Requires
    
    * comment out FileIO stuff, not stable yet

--- src/DrWatson.jl
+++ src/DrWatson.jl
@@ -4,6 +4,16 @@ using Pkg
 
 include("project_setup.jl")
 include("naming.jl")
-# include("saving.jl")
+include("saving.jl")
+
+# Functionality that requires FileIO:
+# using Requires
+# function __init__()
+#     @require FileIO = "5789e2e9-d7fb-5bc7-8068-2c6fae9b9549" begin
+#         import .FileIO: load, save
+#         include("saving_fileio.jl")
+#     end
+# end
+
 
 end
