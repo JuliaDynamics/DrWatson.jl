13764b221a872257c1e8d9cf0549e2f88884e61e
George Datseris <datseris.george@gmail.com>
2019-02-21 20:10:38 +0100

    Use Jonas solution for dict_list

--- src/saving.jl
+++ src/saving.jl
@@ -1,3 +1,6 @@
+export current_commit
+export dict_list, ntuple_list
+
 function addrun! end
 
 function current_commit(path = projectdir())
@@ -16,10 +19,13 @@ end
 
 """
     dict_list(d)
-Take the dictionary `d` and expand it into a vector of dictionaries.
+Expand the dictionary `d` into a vector of dictionaries.
 Each entry has a unique combination from the product of the `Vector`
 values of the dictionary while the non-`Vector` values are kept constant
 for all possibilities. The keys of the entries are the same.
+
+Whether the values of `d` are iterable or not is of no concern;
+the function considers as "iterable" only subtypes of `Vector`.
 
 ## Examples
 julia> d = Dict(:a => [1, 2], :b => 4);
@@ -32,29 +38,38 @@ julia> dict_list(d)
 julia> d[:c] = "test"; d[:d] = ["lala", "lulu"];
 
 julia> dict_list(d)
-6-element Array{Dict{Symbol,Any},1}:
+4-element Array{Dict{Symbol,Any},1}:
  Dict(:a=>1,:b=>4,:d=>"lala",:c=>"test")
  Dict(:a=>2,:b=>4,:d=>"lala",:c=>"test")
  Dict(:a=>1,:b=>4,:d=>"lulu",:c=>"test")
  Dict(:a=>2,:b=>4,:d=>"lulu",:c=>"test")
 
-julia> d[:e] = [[0.1, 0.2], [0.1, 0.3]];
+julia> d[:e] = [[1, 2], [3, 5]];
 
 julia> dict_list(d)
 8-element Array{Dict{Symbol,Any},1}:
- Dict(:a=>1,:b=>4,:d=>"lala",:e=>[0.1, 0.2],:c=>"test")
- Dict(:a=>2,:b=>4,:d=>"lala",:e=>[0.1, 0.2],:c=>"test")
- Dict(:a=>1,:b=>4,:d=>"lulu",:e=>[0.1, 0.2],:c=>"test")
- Dict(:a=>2,:b=>4,:d=>"lulu",:e=>[0.1, 0.2],:c=>"test")
- Dict(:a=>1,:b=>4,:d=>"lala",:e=>[0.1, 0.3],:c=>"test")
- Dict(:a=>2,:b=>4,:d=>"lala",:e=>[0.1, 0.3],:c=>"test")
- Dict(:a=>1,:b=>4,:d=>"lulu",:e=>[0.1, 0.3],:c=>"test")
- Dict(:a=>2,:b=>4,:d=>"lulu",:e=>[0.1, 0.3],:c=>"test")
+ Dict(:a=>1,:b=>4,:d=>"lala",:e=>[1, 2],:c=>"test")
+ Dict(:a=>2,:b=>4,:d=>"lala",:e=>[1, 2],:c=>"test")
+ Dict(:a=>1,:b=>4,:d=>"lulu",:e=>[1, 2],:c=>"test")
+ Dict(:a=>2,:b=>4,:d=>"lulu",:e=>[1, 2],:c=>"test")
+ Dict(:a=>1,:b=>4,:d=>"lala",:e=>[3, 5],:c=>"test")
+ Dict(:a=>2,:b=>4,:d=>"lala",:e=>[3, 5],:c=>"test")
+ Dict(:a=>1,:b=>4,:d=>"lulu",:e=>[3, 5],:c=>"test")
+ Dict(:a=>2,:b=>4,:d=>"lulu",:e=>[3, 5],:c=>"test")
 """
 function dict_list(d)
-    vec(map(Iterators.product(values(d)...)) do vals
-        Dict(keys(d) .=> vals)
-    end)
+    iterable_fields = filter(k -> typeof(d[k]) <: Vector, keys(d))
+    non_iterables = setdiff(keys(d), iterable_fields)
+
+    iterable_dict = Dict(iterable_fields .=> getindex.(Ref(d), iterable_fields))
+    non_iterable_dict = Dict(non_iterables .=> getindex.(Ref(d), non_iterables))
+
+    vec(
+        map(Iterators.product(values(iterable_dict)...)) do vals
+            dd = Dict(keys(iterable_dict) .=> vals)
+            merge(non_iterable_dict, dd)
+        end
+    )
 end
 
 # function namedtuple_list(d::NamedTuple)
