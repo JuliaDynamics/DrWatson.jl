e922bb76b4fa0fb78a3abf8233502636cf22bc91
George Datseris <datseris.george@gmail.com>
2019-02-21 15:11:19 +0100

    Initial dict_list function

--- src/saving.jl
+++ src/saving.jl
@@ -13,3 +13,70 @@ function current_commit(path = projectdir())
     repo = LibGit2.GitRepo(path)
     return string(LibGit2.head_oid(repo))
 end
+
+"""
+    dict_list(d)
+Take the dictionary `d` and expand it into a vector of dictionaries.
+Each entry has a unique combination from the product of the `Vector`
+values of the dictionary while the non-`Vector` values are kept constant
+for all possibilities. The keys of the entries are the same.
+
+## Examples
+julia> d = Dict(:a => [1, 2, 3], :b => 4);
+
+julia> dict_list(d)
+3-element Array{Dict{Symbol,Int64},1}:
+ Dict(:a=>1,:b=>4)
+ Dict(:a=>2,:b=>4)
+ Dict(:a=>3,:b=>4)
+
+julia> d[:c] = "test"; d[:d] = ["lala", "lulu"];
+
+julia> dict_list(d)
+6-element Array{Dict{Symbol,Any},1}:
+ Dict(:a=>1,:b=>4,:d=>"lala",:c=>"test")
+ Dict(:a=>2,:b=>4,:d=>"lala",:c=>"test")
+ Dict(:a=>3,:b=>4,:d=>"lala",:c=>"test")
+ Dict(:a=>1,:b=>4,:d=>"lulu",:c=>"test")
+ Dict(:a=>2,:b=>4,:d=>"lulu",:c=>"test")
+ Dict(:a=>3,:b=>4,:d=>"lulu",:c=>"test")
+
+julia> d[:e] = [[0.1, 0.2]]; # final result retains the inner vector
+
+julia> dict_list(d)
+6-element Array{Dict{Symbol,Any},1}:
+ Dict(:a=>1,:b=>4,:d=>"lala",:e=>[0.1, 0.2],:c=>"test")
+ Dict(:a=>2,:b=>4,:d=>"lala",:e=>[0.1, 0.2],:c=>"test")
+ Dict(:a=>3,:b=>4,:d=>"lala",:e=>[0.1, 0.2],:c=>"test")
+ Dict(:a=>1,:b=>4,:d=>"lulu",:e=>[0.1, 0.2],:c=>"test")
+ Dict(:a=>2,:b=>4,:d=>"lulu",:e=>[0.1, 0.2],:c=>"test")
+ Dict(:a=>3,:b=>4,:d=>"lulu",:e=>[0.1, 0.2],:c=>"test")
+"""
+function dict_list(d)
+    vec(map(Iterators.product(values(d)...)) do vals
+        Dict(keys(d) .=> vals)
+    end)
+end
+
+# function namedtuple_list(d::NamedTuple)
+#     vec(map(Iterators.product(values(d)...)) do vals
+#         s = tuple(keys(d)...)
+#         NamedTuple{s,typeof(vals)}(vals)
+#     end)
+# end
+
+
+# function prepare(cs...)
+#     allkeys = [allaccess(c) for c in cs]
+#
+#     if !allunique(Iterators.flatten(allkeys))
+#         error("The keys of the given containers are not all unique.")
+#     end
+#
+#     # Key type of the final dictionary:
+#     K = promote_type([eltype(k) for k in allkeys]...)
+#     total = Dict{K, Any}()
+#     for (i, acc) in enumerate(allkeys)
+#         c = cs[i]
+#         for k in acc
+#             if eltype(c[k]) <: Vector # Expand
