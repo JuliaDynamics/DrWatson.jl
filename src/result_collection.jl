55dae1dc1ec71b73cdac9bf56cafd645c1a557ef
George Datseris <datseris.george@gmail.com>
2019-04-26 14:03:35 +0200

    add sort(names) to ensure correct comparison for unondered collections

--- src/result_collection.jl
+++ src/result_collection.jl
@@ -120,7 +120,7 @@ but not `df`, a new column will be added and filled with `missing`
 in `df`. Then `df` and `df_new` are concatenated.
 """
 function merge_dataframes!(df1, df2)
-    if names(df1) == names(df2)
+    if sort!(names(df1)) == sort!(names(df2))
         return vcat(df1, df2)
     else
         for m ∈ setdiff(names(df1), names(df2))
