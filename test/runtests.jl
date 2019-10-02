e23e5b24e431408a7f684e62d63d1ca484487ee9
Sebastian Pech <sebastian.pech@me.com>
2019-10-02 22:09:20 +0200

    Group tests to have one output at end

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,11 @@
 using DrWatson, Test
 
-@testset "Naming" begin include("naming_tests.jl"); end
-@testset "Parse savename" begin include("parse_tests.jl"); end
-@testset "Project Setup" begin include("project_tests.jl"); end
-@testset "Saving tools" begin include("stools_tests.jl"); end
-@testset "Produce or Save" begin include("savefiles_tests.jl"); end
-@testset "Collect Results" begin include("update_results_tests.jl"); end
-@testset "Parameter Customization" begin include("customize_savename.jl"); end
+@testset "DrWatson" begin
+    @testset "Naming" begin include("naming_tests.jl"); end
+    @testset "Parse savename" begin include("parse_tests.jl"); end
+    @testset "Project Setup" begin include("project_tests.jl"); end
+    @testset "Saving tools" begin include("stools_tests.jl"); end
+    @testset "Produce or Save" begin include("savefiles_tests.jl"); end
+    @testset "Collect Results" begin include("update_results_tests.jl"); end
+    @testset "Parameter Customization" begin include("customize_savename.jl"); end
+end
