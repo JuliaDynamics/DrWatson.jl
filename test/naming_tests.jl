81645ebcf3c2893b6c73c134d35356e57d80a942
Kai Xu <xukai921110@gmail.com>
2020-02-08 01:47:36 +0000

    fix test

--- test/naming_tests.jl
+++ test/naming_tests.jl
@@ -41,7 +41,7 @@ w = rand(50)
 @test ntuple2dict(@ntuple x y) == @dict x y
 @test sort(collect(keys(dict2ntuple(@dict x y)))) == sort(collect(keys(@ntuple x y)))
 
-@test savename(@ntuple x y z; ignores=(:y)) == "x=3_z=lala"
+@test savename(@ntuple x y z; ignores=(:y,)) == "x=3_z=lala"
 
 a = 3; b = 4
 c = @ntuple a b
